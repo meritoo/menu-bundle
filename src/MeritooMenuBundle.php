f638716cd0327022fabcaa192c4b9989b1170992
Meritoo <github@meritoo.pl>
2019-01-07 20:14:43 +0100

    Implement Phing

---
+++ src/MeritooMenuBundle.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * (c) Meritoo.pl, http://www.meritoo.pl
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Meritoo\MenuBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * The Meritoo Menu Bundle.
+ * Main class.
+ *
+ * @author    Meritoo <github@meritoo.pl>
+ * @copyright Meritoo <http://www.meritoo.pl>
+ */
+class MeritooMenuBundle extends Bundle
+{
+}
---
+++ tests/Resources/config/bundles.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Meritoo\CommonBundle\MeritooCommonBundle::class       => ['all' => true],
+    Meritoo\MenuBundle\MeritooMenuBundle::class           => ['all' => true],
+];
